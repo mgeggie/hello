a7f730206e9c14220fb4ed4ec2c0159aaa2e8606
Mike Geggie <mike@geggie.net>
2021-03-03 23:49:28 -0800

    fix(heroku): make support $PORT environment variable

--- hello.go
+++ hello.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello world.")
 	})
 
-	fmt.Println("Listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Listening on :", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
